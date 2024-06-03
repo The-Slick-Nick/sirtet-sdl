7a82908c7a12fe71c65b651d6d90df332cd8f0b2
The-Slick-Nick <96804604+The-Slick-Nick@users.noreply.github.com>
2024-06-02 22:19:14 -0500

    tests: Add more block tests

--- tests/test_block.c
+++ tests/test_block.c
@@ -6,13 +6,28 @@
 
 void test_rotate_90cw() {
     /* test rotating a block 90 degrees clockwise */
+    Block result;
     Block block1 = {
         .position = {.x = 0, .y = 0},
         .structure = 1
     };
-
-    Block result = Block_rotateCw90(block1);
+    result = Block_rotateCw90(block1);
     ASSERT_EQUAL_INT(result.structure, 1);
+
+    Block block2 = {
+        .position = {.x = 0, .y = 0},
+        .structure = 0
+    };
+    result = Block_rotateCw90(block2);
+    ASSERT_EQUAL_INT(result.structure, 0);
+
+
+    Block block3 = {
+        .position = {.x = 0, .y = 0},
+        .structure = 1 << 0
+    };
+    result = Block_rotateCw90(block3);
+    ASSERT_EQUAL_INT(result.structure, 1 << 3);
 }
 
 
