c24d663b1903406a515fcdf0f7cfb16bc7e3d5ef
The-Slick-Nick <96804604+The-Slick-Nick@users.noreply.github.com>
2024-06-11 06:27:06 -0500

    Set up files for position tracking

---
+++ src/grid.h
@@ -0,0 +1,22 @@
+/* grid.h
+* 
+* Defines the structure and some logic for handling a grid 
+* in which blocks will exist, live, and die.
+*/
+
+#ifndef GRID_H
+#define GRID_H
+
+
+// 10 wide, 20 tall is standard tetris
+#define GRID_HEIGHT 20
+#define GRID_WIDTH 10
+
+
+typedef struct {
+
+
+
+} GameGrid;
+
+#endif
