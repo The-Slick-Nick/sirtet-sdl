d8a14c126281c2b406fec23297b3fbcde6fa163a
The-Slick-Nick <96804604+The-Slick-Nick@users.noreply.github.com>
2024-06-23 21:06:18 -0500

    Include logic to draw grid

--- src/drawing/component_drawing.c
+++ src/drawing/component_drawing.c
@@ -12,6 +12,7 @@
 
 #include "../components/block.h"
 #include "../components/coordinates.h"
+#include "../components/grid.h"
 #include "draw_config.h"
 
 // Lowest-level unit of "draw game component"
@@ -36,6 +37,43 @@ int drawBlockCell(
 
     SDL_SetRenderDrawColor(rend, body_color.r, body_color.g, body_color.g, body_color.a);
     SDL_RenderFillRect(rend, &inner_box);
+
+    return 0;
+}
+
+SDL_Color getCellColorById(int block_id) {
+    return (SDL_Color){.r=155, .g=0, .b=0, .a=255 };
+}
+
+int drawGrid(
+    SDL_Renderer *rend,
+    SDL_Rect display_window,
+    GameGrid *grid
+) {
+
+    int cell_width = display_window.w / grid->width;
+    int cell_height = display_window.h / grid->height;
+
+    for (int row = 0; row < grid->height; row++) {
+        for (int col = 0; col < grid->width; col++) {
+
+            int cell_idx = col + (row * grid->width);
+            int cell_id = *(grid->contents + cell_idx);
+
+            // Standard solution: invalid block IDs are negative
+            if (cell_id < 0) {
+                continue;
+            }
+            drawBlockCell(
+                rend, 
+                (Point){.x=display_window.x + (col * cell_width), .y=display_window.y + (row * cell_height) },
+                cell_width,
+                cell_height,
+                getCellColorById(cell_id),
+                getCellColorById(cell_id) // TODO Introduce some kind of scaling later
+            );
+         }
+    }
 
     return 0;
 }
