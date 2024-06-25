c57e6e409442b18e3892d1f79828696d2f0d1cda
The-Slick-Nick <96804604+The-Slick-Nick@users.noreply.github.com>
2024-06-24 20:51:29 -0500

    Clean up main script in root by moving prototype to src folder

---
+++ build/src/run_game.c.d
@@ -0,0 +1,10 @@
+build/./src/run_game.c.o: src/run_game.c src/components/block.h \
+ src/components/coordinates.h src/drawing/component_drawing.h \
+ src/components/grid.h src/components/block.h \
+ src/components/coordinates.h
+src/components/block.h:
+src/components/coordinates.h:
+src/drawing/component_drawing.h:
+src/components/grid.h:
+src/components/block.h:
+src/components/coordinates.h:
---
+++ src/run_game.h
@@ -0,0 +1,6 @@
+#ifndef run_game_h
+#define run_game_h
+
+int run();
+
+#endif
